d31c11d887c2fcd32628d04383ca2ed4bc8f8b13
ParasRawat29 <parasrawat29pr@gmail.com>
2021-12-19 22:05:04 +0530

    added appear transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import github from "../images/github.png";
 import linkedin from "../images/linkedin.png";
 import twitter from "../images/twitter.png";
 
 export default function Home() {
+  const [appear, setAppear] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAppear(true);
+    }, 200);
+  }, []);
+
   return (
     <div id="home" className={`home `}>
       <div className="anchor"></div>
-      <div className="intro">
+      <div className="intro" style={{ opacity: appear === true ? 1 : 0 }}>
         <h2>Hello World , I'am</h2>
         <h1 className="name">Paras Rawat</h1>
         <h1 className="introInfo">Frontend Developer | UI/UX Developer</h1>
       </div>
-      <div className="socialIcons">
+      <div className="socialIcons" style={{ opacity: appear === true ? 1 : 0 }}>
         <button>
-          <a href="https://github.com/ParasRawat29/" target="_blank">
+          <a
+            href="https://github.com/ParasRawat29/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={github} alt="github icon" width="60" height="60" />
           </a>
         </button>
         <button>
-          <a href="https://www.linkedin.com/in/parasrawat29/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/parasrawat29/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={linkedin} alt="linkedin icon" width="60" height="60" />
           </a>
         </button>
